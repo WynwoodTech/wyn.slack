b805047e08655e3a08abecef332458ae003bd48c
Antonio <antoniom13>
2015-12-26 00:31:33 -0500

    Pushes slack webhook messages to firebase

--- index.js
+++ index.js
@@ -3,20 +3,43 @@
 var config = require('./config/config')
 var firebase = require('./services/firebase');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.get('/', function (req, res){
   res.send('Hello World!');
 });
 
+app.post('/new_message', function (req, res){
+  console.log(req.query);
+
+  var payload = {
+    room:  req.query.channel_name,
+    token: req.query.token,
+    message: req.query.text,
+    username: req.query.user_name,
+    timestamp: req.query.timestamp
+  }
+
+  pushToFirebase(payload);
+
+  res.send('success');
+});
+
+var pushToFirebase = function(payload){
+  var roomRef = firebase.child(payload.room).push().set({
+    message:   payload.message,
+    username:  payload.username,
+    timestamp: payload.timestamp
+  })
+}
+
 var server = app.listen(3002, function (){
   var host = server.address().address;
   var port = server.address().port;
-
-  //firebase.set({
-    //title: "Something",
-    //author: "Me"
-  //})
 
   console.log('Example app listening at http://%s:%s', host, port);
 })
